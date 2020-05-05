0a4f111030b826e0ce89f1daa2d932b0ada5b7fe
setamyDG <setamy.dg@gmail.com>
2020-05-05 18:11:00 +0200

    Add id's of genre_id of each film & deleted unused style

--- components/MovieRow/MovieRow.js
+++ components/MovieRow/MovieRow.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
-import {View, Alert, StyleSheet} from 'react-native';
+import {View, Alert} from 'react-native';
 import CardCustom from '../CardCustom/CardCustom';
 
 class MovieRow extends Component {
   viewMore = () => {
     Alert.alert(
       `PRODUCTION : ${this.props.movie.original_language}`,
-      `DESCRIPTION : ${this.props.movie.overview}\n \n `,
+      `DESCRIPTION : ${this.props.movie.overview}\n \n
+       GENRE : ${this.props.movie.genre_ids}`,
     );
   };
 
@@ -31,11 +32,3 @@ class MovieRow extends Component {
 }
 
 export default MovieRow;
-
-const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
